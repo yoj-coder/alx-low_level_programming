6e8434419d78e27c7ed03fb1f5e4f31537f4809a
oburajoy <oburajoy@gmail.com>
2025-03-28 14:31:40 -0700

    Display sizeof data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,8 @@ long long int m;
 float f;
 printf("Size of a char:%lu\n", (unsigned long)sizeof(c));
 printf("Size of an int:%lu\n", (unsigned long)sizeof(i));
-printf("Size of a long int:%lu\n",(unsigned long)sizeof(li));
+printf("Size of a long int:%lu\n", (unsigned long)sizeof(li));
+printf("Size of a long long int:%lu\n", (unsigned long)sizeof(li));
 printf("Size of a float:%lu\n", (unsigned long)sizeof(f));
 return (0);
 }

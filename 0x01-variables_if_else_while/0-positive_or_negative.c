478a1750a196eedd52221affa2d2e6d2a09e35b3
oburajoy <oburajoy@gmail.com>
2025-04-24 01:28:38 -0700

    Positive or Negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n > 0)
+	if (n > 0)
 
 	{
 	printf("%d is positive\n", n);
 	}
 
-	else if(n == 0)
+	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
@@ -23,6 +23,6 @@ int main(void)
 	{
 	printf("%d is negative\n", n);
 	}
-	return 0;
+	return (0);
 
 }

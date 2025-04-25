5ec2cf55e1a140ee9e1c2fe09a67d9ea52b034fc
oburajoy <oburajoy@gmail.com>
2025-04-25 01:17:10 -0700

    Base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - Prints single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+	{
+	printf("%d \n", i);
+	}
+
+	return (0);
+}
+

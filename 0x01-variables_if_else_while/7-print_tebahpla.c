33501fea38278578e609b176c357c467126e2308
oburajoy <oburajoy@gmail.com>
2025-04-25 02:34:19 -0700

    Print in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Print in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'z';
+	
+	for (i = 'z'; i >= 'a'; i--)
+	{
+	putchar (i);
+	}
+
+	putchar ('\n');
+	return (0);
+}
+

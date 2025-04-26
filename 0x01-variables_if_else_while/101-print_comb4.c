89d455894362d545cb76d7e48e77ec3e16039286
oburajoy <oburajoy@gmail.com>
2025-04-25 22:37:39 -0700

    Prints three digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,13 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens == hundreds) 
+				|| (tens > ones) || (hundreds > tens)))
 				{
 					putchar (hundreds);
 					putchar (tens);
 					putchar (ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+				if (!(ones == '9' && hundreds == '7' && tens == '8'))
 					{
 						putchar (',');
 						putchar (' ');
@@ -30,5 +31,6 @@ int main(void)
 			}
 		}
 	}
+	putchar ('\n');
 	return (0);
 }
